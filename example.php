9614ab517613bee415bb8107d2e1e4be9a4fd2a5
szaffo <martin77szabo@gmail.com>
2017-05-14 20:32:36 +0200

    design example

---
+++ example.php
@@ -0,0 +1,8 @@
+<?php 
+	//this is an example
+	while (code()) {
+		drinkCoffe();
+		$lines += 1;
+		remove('bugs');
+	}
+ ?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<!-- Chrome, Firefox OS and Opera -->
+		<meta name="theme-color" content="black">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="black">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+		<title>/codeMotivate</title>
+		<!-- LINK FAVICON LATER -->
+		<link rel="stylesheet" href="css/main.css">
+		
+		<!-- this will be linked with php -->
+		<link rel="stylesheet" href="css/test.css">
+
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" >
+		<meta charset="utf-8" >
+
+		<!-- for mobile view -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	
+		<!-- Sublime text LiveReload plugin -->
+		<!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?LR-verbose&snipver=1&LiveTest=1"></' + 'script>')</script>		   	 -->
+
+	</head>
+	
+	<body>
+
+		<article class="main-column">
+			<section class="row logo"><h1>tob.hu</h1><h2>/codemotivation</h2></section>
+			<section class="row lang">PHP</section>
+			<section class="row code" id="test">
+				<line><gitter>1</gitter><code><keyword>&#60;?php</keyword></code></line>
+				<line><gitter>2</gitter><code><tab></tab><comment>this is an example</comment></code></line>
+				<line><gitter>3</gitter><code><tab></tab><purple>while </purple><keyword>(</keyword><blue>code</blue><keyword>)) {</keyword></code></line>
+				<line><gitter>4</gitter><code><tab></tab><tab></tab><blue>drinkCooffe</blue><keyword>();</keyword></code></line>
+				<line><gitter>5</gitter><code><tab></tab><tab></tab><variable>lines</variable><keyword> += </keyword><number>1</number><keyword>;</keyword></code></line>
+				<line><gitter>6</gitter><code><tab></tab><tab></tab><blue>remove</blue><keyword>(</keyword><string>bugs</string><keyword>);</keyword></code></line>
+				<line><gitter>7</gitter><code><tab></tab><keyword>}</keyword></code></line>
+				<line><gitter>8</gitter><code><keyword>?&#62;</keyword></code></line>
+			</section>
+		</article>
+
+	</body>
+</html>
